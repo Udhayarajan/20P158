c2cf048e7ae1a622d1ff622d6383b7f993a32bb5
udhayarajan <mudhayarajan.2013@gmail.com>
2023-03-02 20:35:14 +0530

    Added response and still prefix is pending

--- problem2/src/main/kotlin/com/affordmed/problem2/controller/ApiController.kt
+++ problem2/src/main/kotlin/com/affordmed/problem2/controller/ApiController.kt
@@ -1,5 +1,6 @@
 package com.affordmed.problem2.controller
 
+import com.affordmed.problem2.model.Response
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -10,15 +11,49 @@ import org.springframework.web.bind.annotation.RestController
  * Created on 02-03-2023
  */
 
-private val dataset = arrayOf("java", "kotlin", "typescript", "javascript")
+private val dataset = arrayOf(
+    "Java",
+    "Python",
+    "C++",
+    "JavaScript",
+    "Swift",
+    "Kotlin",
+    "Ruby",
+    "PHP",
+    "Go",
+    "Scala",
+    "R",
+    "SQL",
+    "Perl",
+    "Lua",
+    "Objective-C",
+    "TypeScript",
+    "Haskell",
+    "F#",
+    "Dart",
+    "Rust",
+    "Visual Basic",
+    "Assembly",
+    "COBOL",
+    "Fortran",
+    "Lisp"
+)
+private const val FOUND = "found"
+private const val NOT_FOUND = "not_found"
+
 @RestController
 class ApiController {
 
     @GetMapping("prefixes")
     fun prefixes(
         @RequestParam("keywords") keywordString: String
-    ){
+    ) {
         val keywords = keywordString.split(".")
-
+        val result = keywords.map { keyword ->
+            if (keyword in dataset)
+                Response(keyword, FOUND, "")
+            else
+                Response(keyword, NOT_FOUND, "")
+        }
     }
 }
---
+++ problem2/src/main/kotlin/com/affordmed/problem2/model/Response.kt
@@ -0,0 +1,9 @@
+package com.affordmed.problem2.model
+
+
+/**
+ * @author Udhaya
+ * Created on 02-03-2023
+ */
+
+data class Response(val keyword: String, val status: String, val prefix: String)
